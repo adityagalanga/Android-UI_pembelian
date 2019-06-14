064c30e8c0a00d23736b34723f97a3ff0db79a27
Aditya Galang A <adityagalang80@gmail.com>
2019-06-14 11:13:17 +0700

    ver 1

---
+++ app/src/main/java/co/fira/adit/MainActivity.java
@@ -0,0 +1,26 @@
+package co.fira.adit;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String STATE_RESULT = "";
+    EditText a;
+    Button btndial;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setTitle("Google Pixel");
+        }
+    }
+
+
+
+}
